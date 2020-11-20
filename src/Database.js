1d73ff318b22cbec995c29e7db9e87623f52ee5a
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-11-19 23:05:53 -0300

    responsive header

--- src/Database.js
+++ src/Database.js
@@ -11,7 +11,8 @@ export default {
             {
                 slug: 'documentinfos',
                 title: 'ReactJS: A JavaScript library for building user interfaces',
-                date: 'July 17, 2020'
+                date: 'July 17, 2020',
+                image: 'https://programadoresbrasil.com.br/wp-content/uploads/2020/07/reactjs-logo-1-scaled.jpg'
             }
         ]
     }
