e23a8c5dbb5a2ce4edab995d259fdd68fd765d4f
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-11-19 22:41:22 -0300

    header style

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+export default () => {
+    return (
+        <header className="header">
+            <h1 className="header--name">Rodrigo</h1>
+            <h2 className="header--followers">357 followers</h2>
+        </header>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './App.css';
+import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
