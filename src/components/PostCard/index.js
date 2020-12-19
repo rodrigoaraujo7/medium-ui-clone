6a489ebb02815a95f41800f0b696ea51d1224d7b
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-12-19 01:32:25 -0300

    Post Card Transition

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './PostCard.css';
+import '../Animations/Animations.css'
 
-export default ({display}) => {
+export default ({ display }) => {
     return (
-        <div className={display ? 'postcard' : 'postcard postcard--none'}>
+        <div className={display ? 'postcard opacityIn' : 'postcard postcard--none'}>
             <h2>WRITTEN BY</h2>
             <h1>Rodrigo</h1>
             <p>Front-end developer</p>
